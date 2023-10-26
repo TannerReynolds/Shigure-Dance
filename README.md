a036c70e9b282348209739bf9d1784e41945829c
Tanner <Tanner@NorthPeak.Software>
2023-10-25 22:54:27 -0700

    added the dancey dance

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ![danceydance](https://cdn.tokyo.jp/tOVLW.gif)
 Want a little chibi Shigure to dance on your screen? 
 This little program will put just that on your screen above everything else. You can drag her around to anywhere on your screen, and once you got her right where you want her, just press (ctrl+B). Pressing this button will lock her in place and also let you interact with any programs underneath her. Want to move her around again? Just press (ctrl+L) and she'll let you move her again
+
+### How to run
+
+For some reason compiling to a binary messes it up and causes a title bar to display when it's not in focus. So, to run it without issues, you need to install node.js. Once you have node.js installed, download this code and open a command prompt in the src folder. Run the command `npm i`, and when that's finished you can launch the app with `electron .`
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+
+  globalShortcut.register("CommandOrControl+L", () => {
+    mainWindow.setIgnoreMouseEvents(false);
+  });
+  globalShortcut.register("CommandOrControl+B", () => {
+    mainWindow.setIgnoreMouseEvents(true);
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on("will-quit", () => {
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll();
+});
